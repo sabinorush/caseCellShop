86d19cd0d6bc800ca3d46efbb3dd811285fd1a5d
Gustavo Sabino <gu.sabino@hotmail.com>
2026-09-16 07:54:12 -0300

    Refactor: configurar vite para carregar envs de forma dinamica.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,16 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-})
+  };
+});
